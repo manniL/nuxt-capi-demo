b4bd10658f642eec5cf628851a774a4253d82878
Alexander Lichter <github@lichter.io>
2021-06-10 16:42:36 +0200

    chore: setup basic options api app

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,51 @@
 <template>
   <div>
-    Hallo!
+    <h1>Funny Dad Joke</h1>
+    <p>
+      {{ joke }}
+    </p>
+
+    <h2>Old jokes</h2>
+    <ul>
+      <li v-for="oldJoke in oldJokes">
+        {{ oldJoke }}
+      </li>
+    </ul>
+    <button @click="getSuperDadJoke">
+      Click here to get a new joke
+    </button>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'Index',
+  data () {
+    return {
+      jokes: []
+    }
+  },
+  computed: {
+    joke () {
+      return this.jokes[0] ?? 'Click the button to see an awesome joke!!!!!!!!!!1111^^w3q2'
+    },
+    oldJokes () {
+      return this.jokes.slice(1, 5)
+    }
+  },
+  methods: {
+    async getSuperDadJoke () {
+      const headers = {
+        Accept: 'application/json'
+      }
+      const result = await fetch('https://icanhazdadjoke.com/', {
+        headers
+      }).then(r => r.json())
+
+      if (result.joke) {
+        this.jokes.unshift(result.joke)
+      }
+    }
+  }
+}
+</script>
